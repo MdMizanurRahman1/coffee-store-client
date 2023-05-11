48e576e592b9fdef02f27368d695d65fb484adb6
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-05-12 01:20:37 +0300

    delete practiced

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,42 @@
+import Swal from "sweetalert2";
 
 
 const CoffeeCard = ({ coffee }) => {
-    const { name, quantity, supplier, taste, category, details, photo } = coffee;
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your coffee has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
+
+
+
     return (
         <div className="card card-side shadow-xl bg-[#F5F4F1]">
             <figure><img src={photo} alt="Movie" /></figure>
@@ -16,7 +51,7 @@ const CoffeeCard = ({ coffee }) => {
                     <div className="btn-group btn-group-vertical space-y-4">
                         <button className="btn btn-active">View</button>
                         <button className="btn">Edit</button>
-                        <button className="btn">Delete</button>
+                        <button onClick={() => handleDelete(_id)} className="btn">Delete</button>
                     </div>
                 </div>
             </div>
